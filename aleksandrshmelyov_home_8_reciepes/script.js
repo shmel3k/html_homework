7e1b263495ecf75f242b05aa857e67035cff0a68
shmel3k <ashmel@inbox.lv>
2025-01-15 16:51:13 +0200

    GIT + RECEPIES DONE

--- aleksandrshmelyov_home_8_reciepes/script.js
+++ aleksandrshmelyov_home_8_reciepes/script.js
@@ -1,43 +1,85 @@
-
 function init() {
+
+    const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?";
+
     const form = document.getElementById("recipeForm");
-    form.addEventListener("submit", async (event) => {
+    const recipeName = document.getElementById('recipe');
+    const resultDiv = document.querySelector('.result');
+
+    const alphaButtons = document.querySelectorAll('.alphaButton');
+
+    // Search by name
+    form.addEventListener("submit", (event) => {
         event.preventDefault();
-
-        const recipeName = document.getElementById('recipe');
-        const resultDiv = document.querySelector('.result');
-
+        
+        resultDiv.innerHTML = "";
         if (!recipeName || !resultDiv) {
             console.log("___Error", "Username or result not found");
             return;
-        } 
+        }
 
         const recipeNameValue = recipeName.value.trim();
-
-        resultDiv.innerHTML = "";
 
         if (recipeNameValue === "") {
             resultDiv.innerHTML = "Please enter a recipe name";
             return;
         }
 
-        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipeNameValue}`);
-
-        if(!response.ok) {
-            if (response.status === 404) {
-                resultDiv.innerHTML = "Recipe not found";    
-            } else {
-                resultDiv.innerHTML = "An error occurred";
-            }
-            return;
+        if (recipeNameValue) {
+            fetch(`${apiUrl}s=${recipeNameValue}`)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json().then(data => {
+                            if (data.meals) {
+                                recipeResult(data.meals);
+                            } else {
+                                resultDiv.innerHTML = "No recipes found";
+                            }
+                        })
+                    } else {
+                        if (response.status === 404) {
+                            resultDiv.innerHTML = "User not found";
+                        } else {
+                            resultDiv.innerHTML = "An error occurred";
+                        }
+                        return;
+                    }
+                })
         }
+    });
     
-        const recipe = await response.json();
+    // Search by alphabet
+    alphaButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault();
+            resultDiv.innerHTML = "";
+            const letter = button.textContent;
+            fetch(`${apiUrl}f=${letter}`)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json().then(data => {
+                            if (data.meals) {
+                                recipeResult(data.meals);
+                            } else {
+                                resultDiv.innerHTML = "No recipes found";
+                            }
+                        })
+                    } else {
+                        if (response.status === 404) {
+                            resultDiv.innerHTML = "User not found";
+                        } else {
+                            resultDiv.innerHTML = "An error occurred";
+                        }
+                        return;
+                    }
+                })
+        });
+    });
 
-        resultDiv.innerHTML = "";
-
-        if (recipe.meals) {
-            recipe.meals.forEach(meal => {
+    // Function to display recipe results
+    function recipeResult(recipes) {
+        if (recipes) {
+            recipes.forEach(meal => {
                 const recipeDiv = document.createElement('div');
                 recipeDiv.innerHTML = `
                     <h2>${meal.strMeal}</h2>
@@ -46,12 +88,12 @@ function init() {
                     <p>Category: ${meal.strCategory}</p>
                     <p>Area: ${meal.strArea}</p>
                 `;
-                resultDiv.appendChild(recipeDiv);
+                if (resultDiv) {
+                    resultDiv.appendChild(recipeDiv);
+                }
             });
         }
-
     }
-
-);
+    
 }
 document.addEventListener("DOMContentLoaded", init);
